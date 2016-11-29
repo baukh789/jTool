060e849af631645486e3497eed628ace3feaefbc
hjzheng <zhenghaoju700@163.com>
2016-11-29 09:39:13 +0800

    test Element

--- src/Element.js
+++ src/Element.js
@@ -9,29 +9,33 @@ var _Element = {
 
 	// 获取指定索引的jTool对象
 	eq: function(index){
-		return Sizzle(this.DOMList[index]);
+		return new Sizzle(this.DOMList[index]);
 	},
 
 	// 返回指定选择器的jTool对象
 	find: function(selectText) {
-		return Sizzle(selectText, this);
+		return new Sizzle(selectText, this);
 	},
 
 	// 获取与 th 同列的 td jTool 对象, 该方法的调用者只允许为 Th
 	getRowTd: function() {
 		var th = this.eq(0);
-		if(th.get(0).tagName !== 'TH'){
+		if (th.get(0).tagName.toUpperCase() !== 'TH') {
 			utilities.error('getRowTd的调用者只允许为Th');
 			return;
 		}
+
 		var table = th.closest('table'),
-			trList = $('tbody tr', table);
+			trList = new Sizzle('tbody tr', table);
+
 		var tdList = [],
 			thIndex = th.index();
-		utilities.each(trList, function (i, v) {
-			tdList.push(Sizzle('td', v).get(thIndex));
+
+		utilities.each(trList, function(i, v) {
+			tdList.push(new Sizzle('td', v).get(thIndex));
 		});
-		return Sizzle(tdList);
+
+		return new Sizzle(tdList);
 	}
 };
 
